0c5a8033a4a021907a5f57a635fc7ce4f7627159
Fawaz v <fawazchemmad@gmail.com>
2025-02-01 16:37:24 +0530

    updated index.js

--- index.js
+++ index.js
@@ -6,10 +6,6 @@ app.get("/", (req, res) => {
   res.send('{ "response": "Hello From fawaz" }');
 });
 
-app.get("/test", (req, res) => {
-  res.send('{ "response": "welcome" }');
-});
-
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
